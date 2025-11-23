d8e823323ceb4f7919c81dfc6600366984aabba3
Maria Paula Alba <malba@icontec.org>
2025-11-23 14:34:06 -0500

    Creacion API POST/ask y GET/health

--- app/routers/response.py
+++ app/routers/response.py
@@ -3,8 +3,14 @@ from app.pipelines.response_pipeline import QueryResponse
 
 response_bp = Blueprint("response",__name__)
 
+
+
 @response_bp.route('/ask', methods=['POST'])
-def ask():   
+def ask():  
+    """
+    API POST/ask 
+    Recibe la pregunta del usuario y envia la respuesta del asistente con el contexto.
+    """ 
     data = request.get_json(silent=True)
     if not data:
         return jsonify({
@@ -14,12 +20,17 @@ def ask():
     question = data.get("question")
     response = QueryResponse()
     retriver_doc = response.retriver(query_user=question)
-
     context_chunks = [doc["document"] for doc in retriver_doc]
     answer = response.response(query_user=question, retrieved_docs=retriver_doc)
-
 
     return jsonify({
         "answer": answer,
         "context": context_chunks
     }) 
+
+@response_bp.route('/health', methods=['GET'])
+def health():
+
+    return jsonify({
+        "status": "OK"
+    })
